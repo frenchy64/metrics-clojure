14176d62193346c5ec8a77e3ee7499c33382dbf2
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-02-11 13:33:33 -0500

    threatgrid 2.10.1

--- metrics-clojure-riemann/project.clj
+++ metrics-clojure-riemann/project.clj
@@ -1,7 +1,7 @@
-(defproject metrics-clojure-riemann "3.0.0-SNAPSHOT"
+(defproject threatgrid/metrics-clojure-riemann "2.10.1" #_"3.0.0-SNAPSHOT"
   :description "Riemann reporter integration for metrics-clojure"
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
   :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
-  :dependencies [[metrics-clojure "3.0.0-SNAPSHOT"]
+  :dependencies [[metrics-clojure "2.10.0" #_"3.0.0-SNAPSHOT"]
                  [io.riemann/metrics3-riemann-reporter "0.4.6"]])
